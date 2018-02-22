ccec29f2f9e1d4a61c44a98351841721aaaaf981
howanu <alex_krowitz@yahoo.com>
2018-02-21 21:03:19 -0500

    Passes simple test

--- re_root_symlinks.py
+++ re_root_symlinks.py
@@ -11,12 +11,14 @@ import os
 from pathlib import Path
 
 def silent_remove(pathname):
+    print ("Removing "+pathname)
     with contextlib.suppress(FileNotFoundError):
         os.remove(pathname)
 
 def re_root_symlink(link_from,link_to,new_root):
-    silent_remove(link_to)
-    new_link_to = os.path.join(new_root, link_to)
+    silent_remove(link_from)
+    new_link_to = os.path.join(new_root, link_to[1:])
+    print ("ln -s {} {}".format(new_link_to, link_from))
     os.symlink(new_link_to, link_from)
  
 def symlinks(top_dir):
@@ -27,17 +29,20 @@ def symlinks(top_dir):
     for dirName, subdirList, fileList in os.walk(top_dir):
         for file in subdirList + fileList:
             pathname = os.path.join(dirName,file)
+            print("Trying " + pathname)
             path = Path(pathname)
-            if path.is_symlink(pathname):
+            print(path)
+            if path.is_symlink():
                 destination_path = path.resolve()
                 if destination_path.root == "/":
                     link_dict[pathname] = destination_path.as_posix()
+    return link_dict
 
 def re_root_symlinks(top_dir, new_root):
     """
     Change all symlinks that point to root ("/"), so they point to new_root
     """
-    for link_from,link_to in symlinks(top_dir):
+    for link_from,link_to in symlinks(top_dir).items():
         re_root_symlink(link_from,link_to,new_root)
 
 # Should be called after "cd new_root"
